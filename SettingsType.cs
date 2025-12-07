e8f22ec39e9f15a768f77c107e56bb02a6a6c26a
Rodión Romanovich Tabares Correa <37970518+CoKeFish@users.noreply.github.com>
2025-12-07 12:38:48 -0500

    style: improve XML documentation formatting in SettingsType and asmdef files

--- SettingsType.cs
+++ SettingsType.cs
@@ -1,43 +1,45 @@
 ﻿namespace Marmary.SettingsSystem
 {
     /// <summary>
-    /// Represents the setting type for vertical synchronization (VSync).
-    /// VSync is used to control the synchronization of the frame rate with the refresh rate
-    /// of the display to reduce screen tearing.
+    ///     Represents the setting type for vertical synchronization (VSync).
+    ///     VSync is used to control the synchronization of the frame rate with the refresh rate
+    ///     of the display to reduce screen tearing.
     /// </summary>
     public enum SettingsType
     {
         /// <summary>
-        /// Represents the setting type used to configure the frame rate of an application.
+        ///     Represents the setting type used to configure the frame rate of an application.
         /// </summary>
         FrameRate,
 
         /// <summary>
-        /// Represents the fullscreen setting of the application.
-        /// This value determines whether the application runs in fullscreen mode.
+        ///     Represents the fullscreen setting of the application.
+        ///     This value determines whether the application runs in fullscreen mode.
         /// </summary>
         Fullscreen,
 
         /// <summary>
-        /// Represents the setting type used to configure the language preferences of an application.
+        ///     Represents the setting type used to configure the language preferences of an application.
         /// </summary>
         Language,
 
         /// <summary>
-        /// Represents the
+        ///     Represents the setting type used to configure the resolution of the application.
+        ///     Resolution determines the dimensions of the application's display in terms of width and height.
+        /// </summary>
         Resolution,
 
         /// <summary>
-        /// Represents the setting type used to enable or disable vertical synchronization (VSync).
-        /// VSync helps to synchronize the application's frame rate with the display's refresh rate,
-        /// minimizing visual artifacts like screen tearing.
+        ///     Represents the setting type used to enable or disable vertical synchronization (VSync).
+        ///     VSync helps to synchronize the application's frame rate with the display's refresh rate,
+        ///     minimizing visual artifacts like screen tearing.
         /// </summary>
         VSync,
 
         /// <summary>
-        /// Represents the setting type for controlling the master volume level in the system.
-        /// This setting typically adjusts the overall audio output for the application.
+        ///     Represents the setting type for controlling the master volume level in the system.
+        ///     This setting typically adjusts the overall audio output for the application.
         /// </summary>
-        MasterVolume,
+        MasterVolume
     }
 }
